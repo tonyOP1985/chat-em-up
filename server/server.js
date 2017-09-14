38e0ea11f6e54b618ca87c752f443fcaa1d79af7
tonyOP1985 <toliverpaull@gmail.com>
2017-09-14 03:27:33 -0400

    modify server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,10 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicPath));
 const port = process.env.PORT || 3000;
 var app = express();
 
-
+app.use(express.static(publicPath));
 
 // app.get('/', (req, res) => {
 //   res.render('index.html');
