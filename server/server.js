3dacb7a64ce1ae98ebe4058edfa2b34b4d484d1c
tonyOP1985 <toliverpaull@gmail.com>
2017-09-14 03:21:08 -0400

    modified server.js

--- server/server.js
+++ server/server.js
@@ -3,10 +3,15 @@ const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 
+app.use(express.static(publicPath));
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(publicPath));
+
+
+// app.get('/', (req, res) => {
+//   res.render('index.html');
+// });
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
