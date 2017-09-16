99c8366b53559c69cf724891be2bd7de5a9ad897
tonyOP1985 <toliverpaull@gmail.com>
2017-09-16 14:38:28 -0400

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: "Gray@fairytail.com",
-    text: "I can't eat thoses!"
-  });
 });
 
 socket.on('disconnect', function() {
